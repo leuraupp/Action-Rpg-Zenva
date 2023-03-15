c289545d71ef55bc5fe2fba503e0075a139440c7
leuraupp <leuraupp@gmail.com>
2023-03-14 22:15:17 -0300

    Lesson 14/03/2023

--- ActionRPG/Assets/Scripts/Player.cs
+++ ActionRPG/Assets/Scripts/Player.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public float moveSpeed;
+    public float jumpForce;
+
+    public Rigidbody rig;
+
+    private void Update() {
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Jump();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Move() {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 dir = transform.right * x + transform.forward * z;
+        dir *= moveSpeed;
+        dir.y = rig.velocity.y;
+
+        rig.velocity = dir;
+    }
+
+    private void Jump() {
+        Debug.Log("pulou?");
+        if (CanJump()) {
+            rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private bool CanJump() {
+        Ray ray = new Ray(transform.position, Vector3.down);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1.1f)) {
+            Debug.Log(hit.collider);
+            return hit.collider != null;
+        }
+
+        return false;
     }
 }
